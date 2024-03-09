34be196735be1f37adff2091e9169370ab685e1d
khalifa-pendrops <dikee5200@gmail.com>
2024-03-09 12:03:50 +0100

    hamburger menu functional

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-
 function toggleBtn() {
   const body = document.body;
   body.classList.toggle("night");
   body.style.color = "gray";
 }
 
-let menuToggle = document.querySelector(".menu-toggle");
-let menuBar = document.querySelector(".menu");
-// let fadeInparagraph = document.querySelectorAll('fade-in span');
-
-menuToggle.addEventListener("click", function () {
-  menuBar.classList.toggle("active");
-});
-
-
-
-function hamburgerToggle() {
-  let icon = document.querySelector('.night-icon');
-  icon.classList.add('hidden');
-  let menu = document.querySelector(".menu");
-  menu.style.display = menu.style.display === "flex" || menu.style.display === "" ? "none" : "flex";
-  menu.classList.toggle('active');
-  
+function clickHamburger() {
+  let hamburger = document.getElementById("hamburger-menu");
+  let menu = document.getElementById("menu-bar");
+  menu.classList.toggle("active");
 }
+
 
